3878152d67a4192937d0aad87a620ec9884d2056
Surya <thakursuryapratap9@gmail.com>
2022-07-30 02:54:21 +0530

    d

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-
+import Navbar from './components/Navbar'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from '../styles/theme'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
 
 export default MyApp
---
+++ styles/theme.jsx
@@ -0,0 +1,13 @@
+import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#000000",
+        },
+        secondary: {
+            main: '#11cb5f',
+        }
+    },
+});
